a7d057f5ab6e2711ec1d4e2ecf274ccab2f5d10d
seoll27 <aladiahfulminar2@gmail.com>
2023-12-20 21:59:57 +0800

    fix(app): app routing needs to be re-configured

---
+++ index.js
@@ -0,0 +1,15 @@
+/**
+ * @format
+ */
+
+import {AppRegistry} from 'react-native';
+import App from './App';
+import {name as appName} from './app.json';
+import {Text, TextInput} from 'react-native';
+
+Text.defaultProps = Text.defaultProps || {};
+Text.defaultProps.maxFontSizeMultiplier = 1.1; // the maximum amount the font size will scale.
+TextInput.defaultProps = Text.defaultProps || {};
+TextInput.defaultProps.maxFontSizeMultiplier = 1.1; // the maximum amount the font size will scale.
+
+AppRegistry.registerComponent(appName, () => App);
