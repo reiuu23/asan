4a119d004da713f8defb852ce4ff89aa573b4b76
Aladiah <aladiahfulminar2@gmail.com>
2023-12-29 18:55:45 +0800

    fix: unfinished bottom tab navigation

--- src/router/BuyerNavigation.js
+++ src/router/BuyerNavigation.js
@@ -3,30 +3,39 @@ import {
   CardStyleInterpolators,
 } from '@react-navigation/stack';
 
-// LitterApp Screens Routes
+import {
+  NavigationContainer,
+  NavigationHelpersContext,
+} from '@react-navigation/native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 
+// Litter Buyer Screens
+
+import BuyerAbout from '../screens/buyers/BuyerAbout';
+import BuyerAnalytics from '../screens/buyers/BuyerAnalytics';
+import BuyerCart from '../screens/buyers/BuyerCart';
+import BuyerChat from '../screens/buyers/BuyerChat';
 import BuyerHome from '../screens/buyers/BuyerHome';
 import BuyerProfile from '../screens/buyers/BuyerProfile';
 
-const Stack = createStackNavigator();
+// Tab Icons
+import LitterTabIcon from '../assets/img/littertablogo.svg';
+import ChatIcon from '../assets/img/chatIcon.svg';
+
+const Tab = createBottomTabNavigator();
+
+const TabGroup = () => {
+  return (
+    <Tab.Navigator initialRouteName="Home">
+      <Tab.Screen name="Home" component={BuyerHome}></Tab.Screen>
+      <Tab.Screen name="Chat" component={BuyerChat}></Tab.Screen>
+      <Tab.Screen name="About" component={BuyerAbout}></Tab.Screen>
+      <Tab.Screen name="Cart" component={BuyerCart}></Tab.Screen>
+      <Tab.Screen name="Analytics" component={BuyerAnalytics}></Tab.Screen>
+    </Tab.Navigator>
+  );
+};
 
 export default function BuyerNavigation() {
-  return (
-    <Stack.Navigator>
-      <Stack.Screen
-        name="BuyerHome"
-        component={BuyerHome}
-        options={{
-          headerShown: false,
-          cardStyleInterpolator: CardStyleInterpolators.forHorizontalIOS,
-        }}></Stack.Screen>
-      <Stack.Screen
-        name="BuyerProfile"
-        component={BuyerProfile}
-        options={{
-          headerShown: false,
-          cardStyleInterpolator: CardStyleInterpolators.forHorizontalIOS,
-        }}></Stack.Screen>
-    </Stack.Navigator>
-  );
+  return <TabGroup></TabGroup>;
 }
