098d623d5f4c62ec384d164f5325bdc966a69d50
Aladiah <aladiahfulminar2@gmail.com>
2024-01-10 23:15:32 +0800

    fix: pending keyboard status listener

--- src/utils/KeyboardEventListener.js
+++ src/utils/KeyboardEventListener.js
@@ -1,16 +1,23 @@
 import {useState} from 'react';
 import {Keyboard} from 'react-native';
 
-export const keyboardShowListener = Keyboard.addListener(
-  'keyboardDidShow',
-  () => {
-    return true;
-  },
-);
+// export const keyboardShowListener = Keyboard.addListener(
+//   'keyboardDidShow',
+//   () => {
+//     return true;
+//   },
+// );
 
-export const keyboardHideListener = Keyboard.addListener(
-  'keyboardDidHide',
-  () => {
-    return false;
-  },
-);
+// export const keyboardHideListener = Keyboard.addListener(
+//   'keyboardDidHide',
+//   () => {
+//     return false;
+//   },
+// );
+
+export const keyboardListener = Keyboard.addListener(keyboardStatus, () => {
+  if (keyboardStatus === 'keyboardDidShow') {
+    return true;
+  }
+  return false;
+});
