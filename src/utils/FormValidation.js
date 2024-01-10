626a269f1d4cd75116c36c70bae429ca9ced2b44
Aladiah <aladiahfulminar2@gmail.com>
2024-01-10 23:10:59 +0800

    refactor: fixed the validation output

--- src/utils/FormValidation.js
+++ src/utils/FormValidation.js
@@ -9,6 +9,6 @@ export const validationSchema = yup.object().shape({
   password: yup
     .string()
     .required('No password provided.')
-    .min(8, 'Password is too short - should be 8 chars minimum.')
+    .min(8, 'Invalid password.')
     .matches(/[a-zA-Z]/, 'Password can only contain Latin letters.'),
 });
