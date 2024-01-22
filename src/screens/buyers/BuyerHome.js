db08630e661406027cae0345979523da6bda2ff8
Aladiah <aladiahfulminar2@gmail.com>
2024-01-22 23:11:02 +0800

    feat(temporary): tested the newly added icons

--- src/screens/buyers/BuyerHome.js
+++ src/screens/buyers/BuyerHome.js
@@ -1,14 +1,35 @@
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, Text, View, TouchableOpacity} from 'react-native';
 import {useContext, useEffect} from 'react';
 import {AuthContext} from '../../context/AuthContext';
 import Svg, {Path} from 'react-native-svg';
-import {ChatIcon, LitterIcon} from '../../components/Icons';
+import {
+  ChatIcon,
+  LitterIcon,
+  CartIcon,
+  HomeIcon,
+  StatsIcon,
+} from '../../components/Icons';
 import React from 'react';
 
 export default function BuyerHome() {
   const {session} = useContext(AuthContext);
   return (
-    <View>
+    <View style={{marginTop: 50}}>
+      <TouchableOpacity>
+        <LitterIcon></LitterIcon>
+      </TouchableOpacity>
+      <TouchableOpacity>
+        <ChatIcon></ChatIcon>
+      </TouchableOpacity>
+      <TouchableOpacity>
+        <HomeIcon></HomeIcon>
+      </TouchableOpacity>
+      <TouchableOpacity>
+        <CartIcon></CartIcon>
+      </TouchableOpacity>
+      <TouchableOpacity>
+        <StatsIcon></StatsIcon>
+      </TouchableOpacity>
       <Text>BuyerHome</Text>
       <Text>Session Token: {JSON.stringify(session)}</Text>
     </View>
