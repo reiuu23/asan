8d82ffd550f6615d79367d1724347745f3f1b85f
Aladiah <aladiahfulminar2@gmail.com>
2024-02-03 23:07:13 +0800

    feat: completed temp scrap details component

--- src/components/Scraps.js
+++ src/components/Scraps.js
@@ -34,8 +34,26 @@ export default function Scraps() {
       {scrapList.map(scrap => {
         return (
           <>
-            <Image></Image>
+            <Image source={require('../assets/img/plasticImg.png')} />
             <Text>{scrap.scrapName}</Text>
+            <View style={styles.scrap_list__scrap_details}>
+              <View>
+                <Text>Size:</Text>
+                <Text>Cost:</Text>
+                <Text>Quantity:</Text>
+              </View>
+              <View>
+                <Text>{scrap.scrapSizeVolume + ' ' + scrap.scrapSizeUnit}</Text>
+                <Text>
+                  {scrap.scrapCostCurrency +
+                    ' ' +
+                    scrap.scrapCostValue +
+                    ' / ' +
+                    'kg'}
+                </Text>
+                <Text>{scrap.scrapQuantity + ' pieces'}</Text>
+              </View>
+            </View>
           </>
         );
       })}
