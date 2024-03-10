e275008a00beed0a33628f55447061657d57d163
reimnjoo <aladiahfulminar2@gmail.com>
2024-03-10 17:30:52 +0800

    feat: almost complete with the tabular design of the stocks list

--- src/screens/buyers/BuyerStocks.js
+++ src/screens/buyers/BuyerStocks.js
@@ -1,6 +1,14 @@
 import {FlatList, SafeAreaView, StyleSheet, Text, View} from 'react-native';
 import React from 'react';
 import LinearGradient from 'react-native-linear-gradient';
+import {
+  CardboardIcon,
+  ClothIcon,
+  ElectronicsIcon,
+  GlassIcon,
+  MetalIcon,
+  PlasticIcon,
+} from '../../components/Icons';
 
 // Stocks Sample Data
 
@@ -8,13 +16,15 @@ const stocks = require('../../data/scraps.json');
 
 const renderItem = ({item}) => {
   return (
-    <View style={{flexDirection: 'row'}}>
-      <View>
-        <Text>1</Text>
-      </View>
-      <View>
-        <Text>2</Text>
-      </View>
+    <View
+      style={{
+        justifyContent: 'center',
+        flexDirection: 'row',
+        width: 50,
+        height: 50,
+        borderWidth: 1,
+      }}>
+      <Text style={{alignSelf: 'center'}}>{item.scrapSizeVolume}</Text>
     </View>
   );
 };
@@ -25,11 +35,60 @@ export default function BuyerStocks() {
       <View style={styles.top_bar__container}>
         <Text style={styles.top_bar__container_header}>Available Stocks</Text>
       </View>
-      <LinearGradient colors={['#F2F2F2', '#3E5A47']} style={{height: '100%'}}>
-        <FlatList
-          data={stocks} // find a way to filter this data first before rendering. (FINAL HINT)
-          renderItem={renderItem}
-          keyExtractor={item => item.scrapID.toString()}></FlatList>
+      <LinearGradient
+        colors={['#F2F2F2', '#3E5A47']}
+        style={{height: '100%', padding: 50}}>
+        <View style={{flexDirection: 'row', borderWidth: 1}}>
+          <View
+            style={{
+              flexDirection: 'column',
+              borderWidth: 1,
+              borderColor: 'blue',
+              alignItems: 'center',
+              flex: 1,
+            }}>
+            <Text>Types</Text>
+            <View style={styles.scrapStock__icons}>
+              <PlasticIcon></PlasticIcon>
+              <Text>Plastic</Text>
+            </View>
+            <View style={styles.scrapStock__icons}>
+              <CardboardIcon></CardboardIcon>
+              <Text>Paper/Cardboard</Text>
+            </View>
+            <View style={styles.scrapStock__icons}>
+              <MetalIcon></MetalIcon>
+              <Text>Metal</Text>
+            </View>
+            <View style={styles.scrapStock__icons}>
+              <GlassIcon></GlassIcon>
+              <Text>Glass</Text>
+            </View>
+            <View style={styles.scrapStock__icons}>
+              <ElectronicsIcon></ElectronicsIcon>
+              <Text>Electronic Waste</Text>
+            </View>
+            <View style={styles.scrapStock__icons}>
+              <ClothIcon></ClothIcon>
+              <Text>Cloth</Text>
+            </View>
+          </View>
+          <View
+            style={{
+              flexDirection: 'column',
+              borderWidth: 1,
+              borderColor: 'blue',
+              alignItems: 'center',
+              flex: 1.5,
+            }}>
+            <Text>Total Weight (kg)</Text>
+            <FlatList
+              data={stocks} // find a way to filter this data first before rendering. (FINAL HINT)
+              renderItem={renderItem}
+              scrollEnabled={false}
+              keyExtractor={item => item.scrapID.toString()}></FlatList>
+          </View>
+        </View>
       </LinearGradient>
     </SafeAreaView>
   );
@@ -49,5 +108,13 @@ const styles = StyleSheet.create({
     color: '#FFFFFF',
     fontFamily: 'Inter-Bold',
     fontSize: 26,
+  },
+  scrapStock__icons: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: '100%',
+    height: 50,
+    borderWidth: 1,
+    marginBottom: 50,
   },
 });
