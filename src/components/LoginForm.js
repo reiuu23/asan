ffd25a11d25d7baa8f49c5bd2f34c9247b6c8ecd
reimnjoo <aladiahfulminar2@gmail.com>
2024-03-14 19:26:06 +0800

    feat: fixed auth flow -- backend still disabled

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -8,6 +8,7 @@ import {
   Alert,
   ScrollView,
   ActivityIndicator,
+  SafeAreaView,
 } from 'react-native';
 import {Formik} from 'formik';
 import {RFValue} from 'react-native-responsive-fontsize';
@@ -17,134 +18,143 @@ import useFetch from '../hooks/useFetch';
 import CheckBox from '@react-native-community/checkbox';
 import uuid from 'react-native-uuid';
 import '../utils/FormValidation';
+import {AsanIcon} from './Icons';
 
-export default function LoginForm() {
+export default function LoginForm({navigation}) {
   // States
-  const {data, loading, error, fetchData} = useFetch(); // Custom useFetch hook
-  const {session, setSession} = useContext(AuthContext); // Session Context
+  const {loading, error, fetchData} = useFetch(); // Custom useFetch hook
+  const {session, setSession, userType} = useContext(AuthContext); // Session Context
   const [toggleCheckBox, setToggleCheckBox] = useState(false);
+
+  console.log('Login UT: ', userType);
 
   // Authentication Handler
 
   // Bypass authentication - for easier development on the main application.
 
-  setSession({token: uuid.v4()}); // (Temporary Code)
+  // setSession({token: uuid.v4()}); // (Temporary Code)
 
-  const handleAuth = async values => {
-    await fetchData('/auth/', 'post', values); // API request to validate the authentication.
-    if (error) console.error('Authentication failed: ', error);
-  };
+  // const handleAuth = async values => {
+  //   await fetchData('/auth/', 'post', values); // API request to validate the authentication.
+  //   if (error) console.error('Authentication failed: ', error);
+  // };
 
   // UseEffect Hook to validate the authentication status (if true, add the session and proceed to the app. Else, pop up the alert box)
 
-  useEffect(() => {
-    console.log('Returned Data: ', data); // Return
+  // useEffect(() => {
+  //   console.log('Returned Data: ', data); // Return
 
-    if (data.hasOwnProperty('authStatus') && data.authStatus === true) {
-      // setSession({token: uuid.v4(), data: data}); // un-comment once frontend is fully developed.
-      console.log('Authentication successful:');
-      console.log('Session Token: ', session);
-    }
+  //   if (data.hasOwnProperty('authStatus') && data.authStatus === true) {
+  //     // setSession({token: uuid.v4(), data: data}); // un-comment once frontend is fully developed.
+  //     console.log('Authentication successful:');
+  //     console.log('Session Token: ', session);
+  //   }
 
-    if (data.hasOwnProperty('authStatus') && data.authStatus === false) {
-      Alert.alert('Invalid credentials!');
-    }
-  }, [data]);
+  //   if (data.hasOwnProperty('authStatus') && data.authStatus === false) {
+  //     Alert.alert('Invalid credentials!');
+  //     setSession({token: uuid.v4()});
+  //   }
+  // }, [data]);
+
+  const handleAuth = value => {
+    return value;
+  };
 
   return (
-    <ScrollView
-      style={{backgroundColor: 'white'}}
-      contentContainerStyle={styles.scrollWrapper}>
-      <View style={styles.headerContainer}>
-        <Text style={styles.loginHeader}>Let's Sign you in.</Text>
-        <Text style={styles.loginSubHeader}>Welcome to Litter Integrated!</Text>
-      </View>
-      <Formik
-        initialValues={{email: '', password: ''}}
-        onSubmit={handleAuth}
-        validationSchema={validationSchema}>
-        {formikProps => (
-          <>
-            <View style={{flex: 1}}>
-              <TextInput
-                style={styles.formInput}
-                onChangeText={formikProps.handleChange('email')}
-                placeholderTextColor={'#8d929f'}
-                placeholder="Enter your email"
-                autoFocus></TextInput>
-              <Text style={styles.formErrorText}>
-                {formikProps.touched.email && formikProps.errors.email}
-              </Text>
-              <TextInput
-                style={styles.formInput}
-                onChangeText={formikProps.handleChange('password')}
-                placeholderTextColor={'#8d929f'}
-                placeholder="Enter your password"
-                secureTextEntry
-                autoFocus></TextInput>
-              <Text style={styles.formErrorText}>
-                {formikProps.touched.password && formikProps.errors.password}
-              </Text>
-            </View>
-            <View style={styles.formOptionContainer}>
-              <View style={styles.checkBoxContainer}>
-                <CheckBox
-                  disabled={false}
-                  value={toggleCheckBox}
-                  onValueChange={newValue => setToggleCheckBox(newValue)}
-                />
-                <Text style={styles.checkBoxText}>Remember Me</Text>
-              </View>
-              <TouchableOpacity onPress={() => console.log('pressed')}>
-                <Text style={styles.forgotPwText}>Forgot Password?</Text>
-              </TouchableOpacity>
-            </View>
-            <View style={styles.footerContainer}>
-              <TouchableOpacity
-                onPress={formikProps.handleSubmit}
-                style={styles.formSubmitBtn}>
-                {loading ? (
-                  <ActivityIndicator size="small"></ActivityIndicator>
-                ) : (
-                  <Text style={styles.formSubmitBtnText}>Sign In</Text>
-                )}
-              </TouchableOpacity>
-              <View style={styles.footerHelpContainer}>
-                <Text style={styles.footerHeaderText}>
-                  Don't have an account?
-                </Text>
-                <TouchableOpacity>
-                  <Text style={styles.registerBtnText}>Sign Up</Text>
-                </TouchableOpacity>
-              </View>
-            </View>
-          </>
-        )}
-      </Formik>
-    </ScrollView>
+    <SafeAreaView style={{backgroundColor: 'white', height: '100%'}}>
+      <ScrollView contentContainerStyle={styles.scrollWrapper}>
+        <View style={{backgroundColor: '#3E5A47', padding: 20}}>
+          <AsanIcon
+            style={{
+              alignSelf: 'center',
+              marginTop: 50,
+              marginBottom: 20,
+            }}></AsanIcon>
+          <View style={styles.headerContainer}>
+            <Text style={styles.loginHeader}>SIGN IN TO YOUR ACCOUNT</Text>
+          </View>
+          <Formik
+            initialValues={{email: '', password: ''}}
+            onSubmit={handleAuth}
+            validationSchema={validationSchema}>
+            {formikProps => (
+              <>
+                <View>
+                  <Text style={styles.formInputHeader}>E-mail</Text>
+                  <TextInput
+                    style={styles.formInput}
+                    onChangeText={formikProps.handleChange('email')}
+                    placeholderTextColor={'#8d929f'}
+                    placeholder="Enter your email"
+                    autoFocus></TextInput>
+                  <Text style={styles.formErrorText}>
+                    {formikProps.touched.email && formikProps.errors.email}
+                  </Text>
+                  <Text style={styles.formInputHeader}>Password</Text>
+                  <TextInput
+                    style={styles.formInput}
+                    onChangeText={formikProps.handleChange('password')}
+                    placeholderTextColor={'#8d929f'}
+                    placeholder="Enter your password"
+                    secureTextEntry
+                    autoFocus></TextInput>
+                  <Text style={styles.formErrorText}>
+                    {formikProps.touched.password &&
+                      formikProps.errors.password}
+                  </Text>
+                </View>
+                <View style={styles.formOptionContainer}>
+                  <TouchableOpacity onPress={() => console.log('pressed')}>
+                    <Text style={styles.forgotPwText}>Forgot Password?</Text>
+                  </TouchableOpacity>
+                </View>
+                <View style={styles.footerContainer}>
+                  <TouchableOpacity
+                    onPress={formikProps.handleSubmit}
+                    style={styles.formSubmitBtn}>
+                    {loading ? (
+                      <ActivityIndicator size="small"></ActivityIndicator>
+                    ) : (
+                      <Text style={styles.formSubmitBtnText}>Sign In</Text>
+                    )}
+                  </TouchableOpacity>
+                </View>
+              </>
+            )}
+          </Formik>
+        </View>
+        <View style={styles.footerHelpContainer}>
+          <Text style={styles.footerHeaderText}>Don't have an account?</Text>
+          <TouchableOpacity
+            onPress={() => {
+              navigation.navigate('Register');
+            }}>
+            <Text style={styles.registerBtnText}>Sign Up</Text>
+          </TouchableOpacity>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   scrollWrapper: {
     // height: '100%',
-    backgroundColor: 'white',
-    padding: 20,
     // justifyContent: 'space-around',
   },
   headerContainer: {
     marginBottom: 50,
   },
   loginHeader: {
-    color: '#46574C',
+    alignSelf: 'center',
+    color: '#F4F5F4',
     flexWrap: 'wrap',
     fontFamily: 'Inter-Bold',
-    fontSize: RFValue(22),
+    fontSize: 20,
     marginTop: 20,
-    width: 300,
   },
   loginSubHeader: {
-    color: '#5D7365',
+    color: '#F4F5F4',
     flexWrap: 'wrap',
     fontFamily: 'Inter-Medium',
     fontSize: RFValue(20),
@@ -154,25 +164,29 @@ const styles = StyleSheet.create({
   formContainer: {
     justifyContent: 'flex-end',
   },
-  formInput: {
-    borderColor: '#eceff0',
-    borderRadius: 10,
-    borderWidth: 1,
-    color: '#5D7365',
+  formInputHeader: {
+    color: '#F4F5F4',
     fontFamily: 'Inter-SemiBold',
-    fontSize: RFValue(14),
-    // marginBottom: 20,
+    fontSize: 13,
+    paddingLeft: 10,
+  },
+  formInput: {
+    borderColor: '#F4F5F4',
+    borderRadius: 10,
+    borderBottomWidth: 1,
+    color: '#F4F5F4',
+    fontFamily: 'Inter-SemiBold',
+    fontSize: 18,
     paddingTop: 10,
     paddingBottom: 10,
-    paddingLeft: 20,
+    paddingLeft: 10,
     paddingRight: 20,
   },
   formOptionContainer: {
     alignItems: 'center',
     flexDirection: 'row',
-    justifyContent: 'space-between',
+    justifyContent: 'center',
     marginBottom: 20,
-    marginTop: 50,
   },
   checkBoxContainer: {
     alignItems: 'center',
@@ -186,19 +200,22 @@ const styles = StyleSheet.create({
     marginBottom: 3,
   },
   forgotPwText: {
-    color: 'darkred',
+    color: '#F4F5F4',
     fontFamily: 'Inter-Bold',
     fontSize: RFValue(14),
+    textDecorationLine: 'underline',
     marginBottom: 3,
   },
   formSubmitBtn: {
     alignItems: 'center',
-    backgroundColor: '#46574C',
+    backgroundColor: '#F4F5F4',
     borderRadius: 10,
+    elevation: 10,
     padding: 20,
+    marginBottom: 20,
   },
   formSubmitBtnText: {
-    color: 'white',
+    color: '#3E5A47',
     fontFamily: 'Inter-SemiBold',
     fontSize: RFValue(16),
   },
@@ -214,7 +231,7 @@ const styles = StyleSheet.create({
     gap: 8,
     justifyContent: 'center',
     padding: 10,
-    marginTop: 10,
+    marginTop: 40,
     marginBottom: 10,
   },
   footerHeaderText: {
@@ -222,8 +239,9 @@ const styles = StyleSheet.create({
     fontSize: RFValue(14),
   },
   registerBtnText: {
-    color: '#46574C',
+    color: '#3E5A47',
     fontFamily: 'Inter-Bold',
     fontSize: RFValue(14),
+    textDecorationLine: 'underline',
   },
 });
