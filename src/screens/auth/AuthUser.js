0981424986ff36969c7e777165d84d4738ee6720
reimnjoo <aladiahfulminar2@gmail.com>
2024-03-14 19:31:29 +0800

    feat: disabled user type logging

--- src/screens/auth/AuthUser.js
+++ src/screens/auth/AuthUser.js
@@ -1,8 +1,14 @@
 import React, {useState, useEffect} from 'react';
 import {AuthContext} from '../../context/AuthContext';
-import LoginForm from '../../components/LoginForm';
+import {
+  createStackNavigator,
+  CardStyleInterpolators,
+} from '@react-navigation/stack';
+import Login from '../../components/LoginForm';
+import Register from '../../components/RegistrationForm';
 import OwnerNavigation from '../../router/OwnerNavigation';
 import BuyerNavigation from '../../router/BuyerNavigation';
+import CustomStackNavigator from '../../utils/CustomStackNavigator';
 
 export default function AuthUser({navigation, route}) {
   // Session and Root
@@ -10,20 +16,39 @@ export default function AuthUser({navigation, route}) {
   const {userType} = route.params;
   const [session, setSession] = useState(null);
 
-  useEffect(() => {
-    console.log('User Type: ', userType);
-    console.log('Session Token: ', session);
-  }, [session, userType]);
+  // useEffect(() => {
+  //   console.log('User Type: ', userType);
+  //   console.log('Session Token: ', session);
+  // }, [session, userType]);
+
+  const options = {
+    headerShown: false,
+    cardStyleInterpolator: CardStyleInterpolators.forHorizontalIOS,
+  };
+
+  const authScreens = [
+    {
+      name: 'Login',
+      component: Login,
+      options,
+    },
+    {
+      name: 'Register',
+      component: Register,
+      options,
+    },
+  ];
 
   return (
     <>
-      <AuthContext.Provider value={{session, setSession}}>
+      <AuthContext.Provider value={{session, setSession, userType}}>
         {userType === 'owner' ? (
           <>
             {session ? (
               <OwnerNavigation></OwnerNavigation>
             ) : (
-              <LoginForm></LoginForm>
+              <CustomStackNavigator
+                screens={authScreens}></CustomStackNavigator>
             )}
           </>
         ) : (
@@ -31,7 +56,8 @@ export default function AuthUser({navigation, route}) {
             {session ? (
               <BuyerNavigation></BuyerNavigation>
             ) : (
-              <LoginForm></LoginForm>
+              <CustomStackNavigator
+                screens={authScreens}></CustomStackNavigator>
             )}
           </>
         )}
