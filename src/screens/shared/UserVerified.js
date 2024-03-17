a34f608e0eddbf9ff31d327cf0cbcd9be75f2811
reimnjoo <aladiahfulminar2@gmail.com>
2024-03-17 23:57:56 +0800

    feat: added verified page for user verification

---
+++ src/screens/shared/UserVerified.js
@@ -0,0 +1,12 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+
+export default function UserVerified() {
+  return (
+    <View>
+      <Text>UserVerified</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
