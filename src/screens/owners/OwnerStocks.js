e92e60a798a4bddd3ff4f3a1b6faf66703c4ed66
reimnjoo <aladiahfulminar2@gmail.com>
2024-03-23 14:11:49 +0800

    feat: added owner stocks boilerplate

---
+++ src/screens/owners/OwnerStocks.js
@@ -0,0 +1,15 @@
+import {
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+import React from 'react';
+import {BackButtonIcon, SearchIconVar} from '../../components/Icons';
+
+export default function OwnerStocks() {
+  return <Text>OwnerStocks</Text>;
+}
+
+const styles = StyleSheet.create({});
