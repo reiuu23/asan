3a8eb9c72ba2a0476c24817b36ef7b7484afc866
reimnjoo <aladiahfulminar2@gmail.com>
2024-03-23 14:19:30 +0800

    feat: minor temporary unofficial changes

--- src/screens/owners/OwnerHome.js
+++ src/screens/owners/OwnerHome.js
@@ -1,16 +1,18 @@
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
 import React, {useContext} from 'react';
 import {AuthContext} from '../../context/AuthContext';
 
-export default function OwnerHome() {
+export default function OwnerHome({navigation, route}) {
   const {session} = useContext(AuthContext);
   return (
     <View>
       <Text>OwnerHome</Text>
       <Text>Session Token: {JSON.stringify(session.email)}</Text>
+      <TouchableOpacity onPress={() => navigation.openDrawer()}>
+        <Text>Press Me</Text>
+      </TouchableOpacity>
     </View>
   );
 }
-
 
 const styles = StyleSheet.create({});
