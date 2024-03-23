61ed43999abc6a4373a0ff52491d6e9a559acca9
reimnjoo <aladiahfulminar2@gmail.com>
2024-03-23 14:17:39 +0800

    feat: added temporary chat feature for testing

--- src/screens/buyers/BuyerChat.js
+++ src/screens/buyers/BuyerChat.js
@@ -1,19 +1,61 @@
-import {StyleSheet, Text, View} from 'react-native';
-import React from 'react';
-import {
-  AsanIcon,
-  BuyerIcon,
-  EllipseSelect,
-  LitterIcon,
-  OwnerIcon,
-} from '../../components/Icons';
+// import {StyleSheet, Text, View} from 'react-native';
+// import React from 'react';
+// import {
+//   AsanIcon,
+//   BuyerIcon,
+//   EllipseSelect,
+//   LitterIcon,
+//   OwnerIcon,
+// } from '../../components/Icons';
+
+// export default function BuyerChat() {
+//   return (
+//     <View>
+//       <LitterIcon></LitterIcon>
+//     </View>
+//   );
+// }
+
+// const styles = StyleSheet.create({});
+
+import React, {useState, useEffect} from 'react';
+import {Text, TextInput, Button, View} from 'react-native';
+import io from 'socket.io-client';
+
+const socket = io('https://1h8ya96ms0nc.share.zrok.io');
 
 export default function BuyerChat() {
+  const [message, setMessage] = useState('');
+  const [receivedMessages, setReceivedMessages] = useState([]);
+
+  useEffect(() => {
+    // Listen for incoming messages
+    socket.on('chat message', msg => {
+      setReceivedMessages(prevMessages => [...prevMessages, msg]);
+    });
+
+    // Clean up the socket connection on component unmount
+    return () => {
+      socket.disconnect();
+    };
+  }, []);
+
+  const sendMessage = () => {
+    socket.emit('chat message', message);
+    setMessage('');
+  };
+
   return (
     <View>
-      <LitterIcon></LitterIcon>
+      {receivedMessages.map((msg, index) => (
+        <Text key={index}>{msg}</Text>
+      ))}
+      <TextInput
+        placeholder="Type your message..."
+        value={message}
+        onChangeText={text => setMessage(text)}
+      />
+      <Button title="Send" onPress={sendMessage} />
     </View>
   );
 }
-
-const styles = StyleSheet.create({});
