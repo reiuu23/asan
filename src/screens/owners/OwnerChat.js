0440498b06928a2d09b358414bb07d4a0840553a
reimnjoo <aladiahfulminar2@gmail.com>
2024-03-23 14:09:44 +0800

    feat: added owner chat section boilerplate

---
+++ src/screens/owners/OwnerChat.js
@@ -0,0 +1,12 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+
+export default function OwnerChat() {
+  return (
+    <View>
+      <Text>OwnerChat</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
