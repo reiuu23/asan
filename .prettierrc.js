3d361d741b0a45eea41b4c23e7bc55d35363dd46
reimnjoo <aladiahfulminar2@gmail.com>
2024-03-29 23:07:12 +0800

    feat: updated prettier format config

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,18 @@
 module.exports = {
-  arrowParens: 'avoid',
-  bracketSameLine: true,
-  bracketSpacing: false,
-  singleQuote: true,
-  trailingComma: 'all',
+    arrowParens: 'avoid',
+    bracketSameLine: true,
+    bracketSpacing: false,
+    singleQuote: true,
+    trailingComma: 'all',
+    tabWidth: 2,
+
+    singleQuote: true,
+    printWidth: 80,
+    proseWrap: 'always',
+    requireConfig: false,
+    useTabs: false,
+    trailingComma: 'none',
+    bracketSpacing: true,
+    jsxBracketSameLine: false,
+    semi: true,
 };
