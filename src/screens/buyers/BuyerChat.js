37027bbe840a3e75120c2c5bb349ece94bb76c4d
seoll27 <aladiahfulminar2@gmail.com>
2024-04-06 22:59:36 +0800

    chore: removed unused code (up for feature revision)

--- src/screens/buyers/BuyerChat.js
+++ src/screens/buyers/BuyerChat.js
@@ -1,61 +1,50 @@
-// import {StyleSheet, Text, View} from 'react-native';
-// import React from 'react';
-// import {
-//   AsanIcon,
-//   BuyerIcon,
-//   EllipseSelect,
-//   LitterIcon,
-//   OwnerIcon,
-// } from '../../components/Icons';
-
-// export default function BuyerChat() {
-//   return (
-//     <View>
-//       <LitterIcon></LitterIcon>
-//     </View>
-//   );
-// }
-
-// const styles = StyleSheet.create({});
-
 import React, {useState, useEffect} from 'react';
-import {Text, TextInput, Button, View} from 'react-native';
+import {ScrollView, Text, View, TextInput, Button} from 'react-native';
 import io from 'socket.io-client';
 
-const socket = io('https://1h8ya96ms0nc.share.zrok.io');
-
-export default function BuyerChat() {
-  const [message, setMessage] = useState('');
-  const [receivedMessages, setReceivedMessages] = useState([]);
+const BuyerChat = () => {
+  const [messages, setMessages] = useState([]);
+  const [inputMessage, setInputMessage] = useState('');
+  const socket = io('http://192.168.100.5:3000');
 
   useEffect(() => {
-    // Listen for incoming messages
+    socket.emit('user type', 'buyer', 1); // Assuming '1' as buyer's user ID
+
     socket.on('chat message', msg => {
-      setReceivedMessages(prevMessages => [...prevMessages, msg]);
+      setMessages(prevMessages => [...prevMessages, msg]);
     });
 
-    // Clean up the socket connection on component unmount
     return () => {
       socket.disconnect();
     };
   }, []);
 
   const sendMessage = () => {
-    socket.emit('chat message', message);
-    setMessage('');
+    socket.emit('chat message', {
+      recipient: 'scrapyard owner',
+      message: inputMessage,
+    }); // Assuming 'scrapyard owner' as recipient type
+    setMessages(prevMessages => [...prevMessages, inputMessage]); // Display the sent message immediately
+    setInputMessage('');
   };
 
   return (
     <View>
-      {receivedMessages.map((msg, index) => (
-        <Text key={index}>{msg}</Text>
-      ))}
-      <TextInput
-        placeholder="Type your message..."
-        value={message}
-        onChangeText={text => setMessage(text)}
-      />
-      <Button title="Send" onPress={sendMessage} />
+      <ScrollView>
+        {messages.map((msg, index) => (
+          <Text key={index}>{msg}</Text>
+        ))}
+      </ScrollView>
+      <View>
+        <TextInput
+          value={inputMessage}
+          onChangeText={setInputMessage}
+          placeholder="Type your message..."
+        />
+        <Button title="Send" onPress={sendMessage} />
+      </View>
     </View>
   );
-}
+};
+
+export default BuyerChat;
